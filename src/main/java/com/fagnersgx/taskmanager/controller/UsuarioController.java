a4ba9cb65bf4d432ec5fa1fa587eac5e6c43f402
FagnerSG <fagnersgx@gmail.com>
2026-06-24 23:17:05 -0300

    atualização e correção dos UsuarioController, UsuarioService e do UsuarioEntity

--- src/main/java/com/fagnersgx/taskmanager/controller/UsuarioController.java
+++ src/main/java/com/fagnersgx/taskmanager/controller/UsuarioController.java
@@ -1,35 +1,33 @@
 package com.fagnersgx.taskmanager.controller;
 
+import com.fagnersgx.taskmanager.dto.UserDto;
+import com.fagnersgx.taskmanager.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fagnersgx.taskmanager.entity.UsuarioEntity;
-import com.fagnersgx.taskmanager.repository.UsuarioRepository;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/v1/usuarios")
 public class UsuarioController {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private UsuarioService usuarioService;
 
-    @Autowired
-    private UserDto userDto;
-
-    @PostMapping(path="/add")
-    public ResponseEntity<UsuarioEntity> createUser(@RequestBody UserDto userDto) {
-        return usuarioRepository.save(userDto);
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
     }
 
-    @GetMapping("/{usuarioId}")
-    public ResponseEntity<UsuarioEntity> getUserByID(@PathVariable("usuarioId") Long usuarioId) {
-        return null;
+    @PostMapping(path="/add")
+    public ResponseEntity<UsuarioEntity> criarUsuario(@RequestBody UserDto createUsuarioDto) {
+        UsuarioEntity novoUsuario = usuarioService.salvar(createUsuarioDto);
+        return ResponseEntity.status(201).body(novoUsuario);
     }
     
 }
--- src/main/java/com/fagnersgx/taskmanager/service/UsuarioService.java
+++ src/main/java/com/fagnersgx/taskmanager/service/UsuarioService.java
@@ -1,9 +1,24 @@
 package com.fagnersgx.taskmanager.service;
 
+import com.fagnersgx.taskmanager.dto.UserDto;
+import com.fagnersgx.taskmanager.entity.UsuarioEntity;
+import com.fagnersgx.taskmanager.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
 
-    public void createUsuario(UserDto userDto) {
-        
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
     }
-    
+
+    public UsuarioEntity salvar(UserDto createUsuarioDto) {
+        UsuarioEntity salvarUsuario = new UsuarioEntity();
+        salvarUsuario.setUsername(createUsuarioDto.username());
+        salvarUsuario.setEmail(createUsuarioDto.email());
+        salvarUsuario.setSenha(createUsuarioDto.senha());
+        return usuarioRepository.save(salvarUsuario);
+    }
 }
